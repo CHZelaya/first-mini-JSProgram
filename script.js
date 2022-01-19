3137bb6dcfded21e023534fd0c30f3f3f2cacb9d
CHZelaya <chzelaya1201@gmail.com>
2022-01-18 20:59:54 -0700

    Updated all files, deleted unneccesary code. Finalized project(for now)

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+// Making Random Number Generator
+
+let randomNumberGenerator = Math.floor( 1 + 5*Math.random() );
+
+
+let guessRandomNumber = () => {
+        // Setting up userGuess
+        function userGuess(correct,incorrect) {
+        var userNumber = Number( document.getElementById("guess").value);
+        document.getElementById("question").value = "";
+        if ( isNaN(userNumber)|| userNumber < 1 || userNumber > 5) {
+            document.getElementById("question").innerHTML = 
+            ('Bad Input! Please try again with an integer between 1 and 5')
+        } else if (userNumber == randomNumberGenerator) correct() 
+        else incorrect();
+        }
+
+        userGuess(
+            function correct() {document.getElementById("question").innerHTML = 
+            (`Congratulations! <br> You guessed correctly!<br>I've picked a new number, Try again!`);
+            randomNumberGenerator = Math.floor( 1 + 5*Math.random() );
+            console.log(randomNumber) },
+            function incorrect() {document.getElementById('question').innerHTML = (
+                `You guessed incorrectly, please try again!`
+            );}
+        );
+}
+
+        // Event Listeners 
+function handle(e){
+    e.preventDefault();
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////
+
+// Original Script, before throwing in all the DOM elements. // 
+
 // function gameGuessRandomNumber(min = 1, max = 10) {
 //      // Setting up random number generator
 //     min = Math.ceil(min);
@@ -21,40 +58,6 @@
 
 // gameGuessRandomNumber();
 
-let randomNumber = Math.floor( 1 + 10*Math.random() );
-
-console.log(randomNumber);
-
-let guessRandomNumber = () => {
-        // Setting up userGuess
-        function userGuess(correct,incorrect) {
-        var userNumber = Number( document.getElementById("guess").value);
-        document.getElementById("question").value = "";
-        if ( isNaN(userNumber)|| userNumber < 1 || userNumber > 10) {
-            document.getElementById("question").innerHTML = 
-            ('Bad Input! Please try again with an integer between 1 and 10')
-        } else if (userNumber == randomNumber) correct() 
-        else incorrect();
-        }
-
-        userGuess(
-            function correct() {document.getElementById("question").innerHTML = 
-            (`Congratulations! <br> You guessed correctly!<br>I've picked a new number, Try again!`);
-            randomNumber = Math.floor( 1 + 10*Math.random() );
-            console.log(randomNumber) },
-            function incorrect() {document.getElementById('question').innerHTML = (
-                `You guessed incorrectly, please try again!`
-            );}
-        );
-}
 
 
-
-
-
-
-        // Event Listeners 
-
-document.getElementById("guess-button").onclick = guessRandomNumber;
-document.getElementById("guess-button").keypress= guessRandomNumber;
 
